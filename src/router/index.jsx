730518208d809931e61360480c71053192ff88a2
Anshu Ingle <anshu.ingle2105@gmail.com>
2026-07-13 01:13:16 +0530

    Added router basename

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,16 +2,21 @@ import { createBrowserRouter } from 'react-router-dom';
 import Layout from '@/components/layout/Layout';
 import Home from '@/pages/Home';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '',
+          element: <Home />,
+        },
+        // Future page routes (e.g. Projects, Experience, Blog) can be added here
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '',
-        element: <Home />,
-      },
-      // Future page routes (e.g. Projects, Experience, Blog) can be added here
-    ],
-  },
-]);
+    basename: '/Anshu-Portfolio',
+  }
+);
